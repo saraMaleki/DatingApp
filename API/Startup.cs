e5d4d13f270a7997230cc154abec806b3ac6987f
Your Name <sarah.maleki83@gmail.com>
2020-11-17 10:07:11 +0330

    commit session 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,6 +43,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -62,6 +63,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
